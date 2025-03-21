da5b9e11c26dab4c4c4b21c53328df9a51f08f9f
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-03-21 18:12:47 +0530

    second push clean

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,24 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  projectId: "u18ku8",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    enableCode:false,
+    embeddedvideos: true,
+    video: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+      specPattern: 'cypress/e2e/**/*.cy.js';
+      excludeSpecPattern: ["**/examples/*", "**/templates/*"];
+      supportFile: 'cypress/support/commands.js';
     },
   },
 });
