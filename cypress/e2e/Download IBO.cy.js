9245cf11a1a17afd2cf4b809d60cfd378832879e
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-08-01 17:06:10 +0530

    Download IBO.cy.js

--- cypress/e2e/Download IBO.cy.js
+++ cypress/e2e/Download IBO.cy.js
@@ -5,26 +5,36 @@ import { login } from './Login Polaris.cy';
 
 describe('Download IBO', () => {
   before(() => {
-    Cypress.on('uncaught:exception', (err, runnable) => {
-      return false;
-    });
+    Cypress.on('uncaught:exception', () => false);
   });
 
   it('performs Download IBO actions', () => {
-    // Call the login function from login.cy.js
     login();
-
     cy.wait(5000);
-    // Download Form
+
+    // Navigation
     cy.xpath("//img[@title='polaris logo']").should('be.visible').click();
     cy.xpath("//div[@class='bond_box_btn_wrap']//a[@class='btn_bond_main'][normalize-space()='Find Bonds']").click();
     cy.xpath("//a[normalize-space()='IBO']").click();
-    cy.xpath("(//a[@title='Apply'])[1]").click({ force: true });
-    cy.wait(3000);
-    cy.xpath("//a[@class='btn popup-button'][normalize-space()='Print']", { timeout: 3000 }).click();
-    cy.xpath("//a[@class='btn popup-button'][normalize-space()='Cancel']").click();
+
+    // Either Apply button or No IBO message
+    cy.get("div[class='item mt-5 pt-5 ng-star-inserted'] h4")
+      .should('exist')
+      .then(($element) => {
+        const text = $element.text().trim();
+
+        if (text === 'Apply') {
+          cy.wrap($element).click({ force: true });
+          cy.wait(3000);
+          cy.xpath("//a[@class='btn popup-button'][normalize-space()='Print']").click();
+          cy.xpath("//a[@class='btn popup-button'][normalize-space()='Cancel']").click();
+        } else if (text === 'NO IBO Available !!') {
+          cy.log('No IBO Available!');
+        }
+      });
+
+    // Continue the rest of the test
     cy.xpath("//a[normalize-space()='Capital Gain 54EC']").click();
     cy.xpath("//a[normalize-space()='RBI Bonds']").click();
-
   });
 });
